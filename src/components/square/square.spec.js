f1ff6acba3d6797c2dae26f14f0376e43f5166aa
assafs <assafs@wix.com>
2018-02-03 23:37:37 +0200

    refactor test

--- src/components/square/square.spec.js
+++ src/components/square/square.spec.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Square } from './square';
 import { SquareDriver } from './Square.driver';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
 
-/*beforeEach(() => {
-  let squareDriver = new SquareDriver();  
-});*/
+let squareDriver;
+
+beforeEach(() => {
+  squareDriver = new SquareDriver();
+});
 
 it('Square hide value of closed Square', () => {
-  let squareDriver = new SquareDriver();
   squareDriver.given.value('2');
   squareDriver.given.isOpen(false);
   squareDriver.build();
@@ -19,7 +16,6 @@ it('Square hide value of closed Square', () => {
 
 
 it('Square show value of open Square', () => {
-  let squareDriver = new SquareDriver();
   squareDriver.given.value('2');
   squareDriver.given.isOpen(true);
   squareDriver.build();
