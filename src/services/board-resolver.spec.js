f42525c88511acc0f2c92f2ef19ebaa0c27262e4
assafs <assafs@wix.com>
2018-02-03 21:59:36 +0200

    board-resolver spec

--- src/services/board-resolver.spec.js
+++ src/services/board-resolver.spec.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import {checkWinState,generateSquares} from './board-resolver'
 import { MINE } from '../constans/consts';
 import { expect } from 'chai';
-import { moynt } from 'enzyme';
 
 /*beforeEach(() => {
   let board = new board();  
@@ -39,5 +36,51 @@ it('Method: generateSquares - generates squares correctly', () => {
   expect(squares.length).to.equal(cols * rows) ;
   expect(minedSquares.length).to.equal(mines) ;
 });
+
+it('should create a board with no mines with the supplied dimensions', () => {
+  let boardConfig = {
+    cols: 2,
+    rows: 1,
+    mines: 0
+  };
+  let expectedBoard = [{ value: 0, isOpen: false }, { value: 0, isOpen: false }];
+  let board = generateSquares(boardConfig);
+  expect(board).to.deep.equal(expectedBoard);
+});
+
+it('should create a board with unrevealed sqaures', () => {
+  let boardConfig = {
+    cols: 1,
+    rows: 1,
+    mines: 0
+  };
+  let board = generateSquares(boardConfig);
+  expect(board[0].isOpen).to.be.false;
+});
+
+it('should create a board with supplied mines', () => {
+  let boardConfig = {
+    cols: 1,
+    rows: 1,
+    mines: 1
+  };
+  let board = generateSquares(boardConfig);
+  expect(board[0].value).to.equal(MINE);
+});
+
+it('should calculate ajustents mine ', () => {
+  let boardConfig = {
+    cols: 2,
+    rows: 2,
+  };
+  let expectedBoard = [{ value: 1, isOpen: false }, { value: 1, isOpen: false }, { value: 1, isOpen: false }, { value: MINE, isOpen: false }];
+  let board = generateSquares(boardConfig);
+  console.log(board);
+  expect(board).to.include.members(expectedBoard);
+});
+
+
+
+
 
 
