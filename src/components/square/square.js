46461753b8e168741a81863ba2e1cce1b473f02e
Shai Ben Valid <shai@slatescience.com>
2018-02-04 21:46:33 +0200

    remove console

--- src/components/square/square.js
+++ src/components/square/square.js
@@ -10,9 +10,6 @@ export const Square = (props) => {
     const className = props.isOpen ? 'square open' : 'square';
     let value = props.isFlaged ? flag() : props.isOpen && props.value || props.superman ? props.value : '';
     value = value === MINE ? bomb() : value;
-    value = value === MINE ? <i className="fas fa-bomb"></i> : value;
-    console.log('isOpen: '+props.isOpen);
-    console.log('value: '+props.value);
     return (
         <button className={className} onClick={(e) => props.onClick(e)}>
             {value}
